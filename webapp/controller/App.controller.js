59f6472085c489b97f77726f08d17467f2eb61d7
Gabriel Bessa <gabriel.bessa@solutions.valantic.com>
2023-07-27 09:12:02 +0200

    third commit

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -8,42 +8,28 @@ sap.ui.define([
 	var PageController = Controller.extend("PdfViewTest.controller.App", {
 
 		onInit: function () {
+			// Versucht, lokale pdf-Dateien zu laden
 			// this._sValidPath = sap.ui.require.toUrl("sap/m/sample/PdfViewTest") + "/sample.pdf"; // sap.ui.core.URI("webapp/src/dummy.pdf") 
 			// this._sInvalidPath = sap.ui.require.toUrl("sap/m/sample/PdfViewTest") + "/sample_nonexisting.pdf";
 			this._oModel = new JSONModel({
-				Source: "http://s29.q4cdn.com/175625835/files/doc_downloads/test.pdf",
+				// Versucht, lokale pdf-Dateien zu laden
+				// Source: this._sValidPath,
 				Title1: "My Title 1",
 				Title2: "My Title 2",
 				Height: "850px"
 			});
 			this.getView().setModel(this._oModel);
 		},
+		// Versucht, lokale pdf-Dateien zu laden
+		// onCorrectPathClick: function () {
+		// 	this._oModel.setProperty("/Source", this._sValidPath);
+		// },
 
-		onCorrectPathClick: function () {
-			this._oModel.setProperty("/Source", this._sValidPath);
-		},
-
-		onIncorrectPathClick: function () {
-			this._oModel.setProperty("/Source", this._sInvalidPath);
-		}
+		// onIncorrectPathClick: function () {
+		// 	this._oModel.setProperty("/Source", this._sInvalidPath);
+		// }
 	});
 
 	return PageController;
 
 });
-
-// sap.ui.define(["sap/ui/core/mvc/Controller"],
-//     function (Controller) {
-//     "use strict";
-
-//     return Controller.extend("PdfViewTest.controller.App", {
-
-//         onInit: function(){
-
-//             var pdfView = this.getView().byId("pdfView");
-//             pdfView.setValue("egg.pdf");
-//         }
-
-//     });
-
-// });
